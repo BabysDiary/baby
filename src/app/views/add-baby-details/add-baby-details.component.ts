df80bcb166e57a1481b65378b589bac788c60974
BabysDiary <babysdiaries@gmail.com>
2018-12-02 21:18:49 +0530

    changes

--- src/app/views/add-baby-details/add-baby-details.component.ts
+++ src/app/views/add-baby-details/add-baby-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-
+import swal from 'sweetalert';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { NzMessageService, UploadFile } from 'ng-zorro-antd';
@@ -10,22 +10,18 @@ import { NzMessageService, UploadFile } from 'ng-zorro-antd';
   templateUrl: './add-baby-details.component.html',
   styleUrls: ['./add-baby-details.component.scss']
 })
-
 export class AddBabyDetailsComponent implements OnInit {
   babyObject: any;
   selectedPath: File = null;
   apiBabyDetails: 'https://babys-diary.herokuapp.com/api/baby-diary';
-  fileList = [
-  ];
+  fileList = [];
   previewImage = '';
   previewVisible = false;
 
   ngOnInit() {
     this.babyObject = {};
   }
-  constructor(private _http: HttpClient,
-  private msg: NzMessageService
-  ) {}
+  constructor(private _http: HttpClient, private msg: NzMessageService) {}
 
   fileSelected(event) {
     this.selectedPath = <File>event.target.files[0];
@@ -47,11 +43,16 @@ export class AddBabyDetailsComponent implements OnInit {
   subscribeBabyDetails(): void {
     this.postBabyDetails(this.babyObject).subscribe(data => {
       console.log('Details Uploaded', data);
-    });
+      swal("Upload Successful", "Baby's details uploaded Successfully !");
+    },
+    (error)=>{
+        swal("Failed to Upload details! Try after sometime!");
+    }
+    );
   }
 
   handlePreview = (file: UploadFile) => {
     this.previewImage = file.url || file.thumbUrl;
     this.previewVisible = true;
-  }
+  };
 }
